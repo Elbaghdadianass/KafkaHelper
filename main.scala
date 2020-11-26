4805aa520d2d2bf078b94fc6fc4c0ea7c93133d1
elbaghdadi anass <39307483+Elbaghdadianass@users.noreply.github.com>
2020-11-26 19:50:15 +0100

    Update main.scala

--- main.scala
+++ main.scala
@@ -1,15 +1,18 @@
+/**
+ * @author elbaghdadianass
+ */
 object main extends App {
   val spark = SparkSession
     .builder()
     .appName("main class")
     .config("spark.master", "local")
     .getOrCreate()
-    
-   val kafkahelper = spark.sparkContext.broadcast(KafkaHelper(props))
-   
-   dstream.foreachRDD { rdd =>
+
+  val kafkahelper = spark.sparkContext.broadcast(KafkaHelper(props))
+
+  dstream.foreachRDD { rdd =>
     rdd.foreach { message =>
       kafkaSink.value.send("topic_name", message)
-                }
-      }
+    }
+  }
 }

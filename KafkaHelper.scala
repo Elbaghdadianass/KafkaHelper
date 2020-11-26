9c1f516e52a83a9ed001772b69ed2569449331a6
elbaghdadi anass <39307483+Elbaghdadianass@users.noreply.github.com>
2020-11-26 18:21:31 +0100

    Add files via upload

---
+++ KafkaHelper.scala
@@ -0,0 +1,33 @@
+package com.ocp.ct.utilities
+
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+/**
+ * @author elbaghdadianass
+ */
+class KafkaHelper(createProducer: () => KafkaProducer[String, String]) extends Serializable {
+
+  @transient lazy val producer = createProducer()
+
+  def send(topic: String, value: String): Unit = producer.send(new ProducerRecord(topic, value))
+}
+
+object KafkaHelper {
+  def apply(config: Properties): KafkaHelper = {
+    val f = () => {
+      val producer = new KafkaProducer[String, String](config)
+
+      sys.addShutdownHook {
+        producer.close()
+      }
+
+      producer
+    }
+    new KafkaHelper(f)
+  }
+}
+
+
+
